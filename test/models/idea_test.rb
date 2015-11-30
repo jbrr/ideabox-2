42e4b056e675c96216a4a11e7cdbad56dcf07b7f
Jeffrey Ruane <ruane.jb@gmail.com>
2015-11-30 14:26:25 -0700

    Start model testing

--- test/models/idea_test.rb
+++ test/models/idea_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class IdeaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  attr_reader :idea
+  def setup
+    @idea = Idea.create(title: "Test",
+                        body: "Test",
+                        quality: 1)
+  end
+
+  test "an idea has a title" do
+    assert_equal idea.title, "Test"
+  end
 end
