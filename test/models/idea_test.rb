1f54f5474e5508699fc41a018d7d25af5aa44332
Jeffrey Ruane <ruane.jb@gmail.com>
2015-11-30 14:29:43 -0700

    Add model test for idea

--- test/models/idea_test.rb
+++ test/models/idea_test.rb
@@ -4,11 +4,18 @@ class IdeaTest < ActiveSupport::TestCase
   attr_reader :idea
   def setup
     @idea = Idea.create(title: "Test",
-                        body: "Test",
-                        quality: 1)
+                        body: "Test")
   end
 
   test "an idea has a title" do
     assert_equal idea.title, "Test"
+  end
+
+  test "an idea has a body" do
+    assert_equal idea.body, "Test"
+  end
+
+  test "an idea has a default quality" do
+    assert_equal idea.quality, 1
   end
 end
