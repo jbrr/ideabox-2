55301a4bd0123cf0200557dbaf6b0d57f404ec6a
Jeffrey Ruane <ruane.jb@gmail.com>
2015-11-30 19:20:31 -0700

    Refactor to move idea ordering to model

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,5 +1,5 @@
 class IdeasController < ApplicationController
   def index
-    @ideas = Idea.all.sort_by(&:created_at).reverse
+    @ideas = Idea.all
   end
 end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,3 +1,4 @@
 class Idea < ActiveRecord::Base
+  default_scope { order("created_at DESC") }
   enum quality: %w(swill plausible genius)
 end
