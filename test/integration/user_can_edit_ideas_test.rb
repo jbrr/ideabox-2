7e0d8462eba19fdb66fc9c33760a58751f582857
Jeffrey Ruane <ruane.jb@gmail.com>
2015-12-02 21:13:57 -0700

    Add test for user editing ideas

---
+++ test/integration/user_can_edit_ideas_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class UserCanEditIdeasTest < ActionDispatch::IntegrationTest
+  def teardown
+    Capybara.reset_sessions!
+  end
+
+  test "user can edit title" do
+    visit "/"
+    title = first(:xpath, "//h5[@contenteditable='true']")
+    title.set("Hello")
+    title.native.send_keys(:return)
+    assert page.has_content?("Hello")
+  end
+
+  test "user can edit body" do
+    visit "/"
+    body = first(:xpath, "//p[@contenteditable='true']")
+    body.set("New body")
+    body.native.send_keys(:return)
+    assert page.has_content?("New body")
+  end
+end
