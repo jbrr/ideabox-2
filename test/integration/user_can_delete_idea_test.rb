80d3150da009a7e10456c4cfacfb08a5dbc48e68
Jeffrey Ruane <ruane.jb@gmail.com>
2015-12-02 22:18:08 -0700

    Try to fix test to solve occasional unknown failures

--- test/integration/user_can_delete_idea_test.rb
+++ test/integration/user_can_delete_idea_test.rb
@@ -7,7 +7,14 @@ class UserCanDeleteIdeaTest < ActionDispatch::IntegrationTest
 
   test "user deletes an idea" do
     visit "/"
+    fill_in("idea-title", with: "New Idea")
+    fill_in("idea-body", with: "New Body")
+    click_on("Create Idea")
+    assert page.has_content?("New Idea")
+    assert page.has_content?("New Body")
+
     first("#delete-idea").click
-    refute page.has_content?("Later Idea")
+    save_and_open_page
+    refute page.has_content?("New Idea")
   end
 end
